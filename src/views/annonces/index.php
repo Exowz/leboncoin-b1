8de9fdb64a485c077f7d10ad6fb7a7cf8d33b6e9
Ewan Kapoor <mke.kapoor@icloud.com>
2024-06-10 16:32:40 +0200

    fichier php nécessaires

--- src/views/annonces/index.php
+++ src/views/annonces/index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Le Petit Coin</title>
     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <header class="header">
